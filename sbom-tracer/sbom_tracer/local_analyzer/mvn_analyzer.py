0aa23181a93c6d5957590ef0baf7426d8688b1da
huanceng <hhq0802@foxmail.com>
2022-11-03 09:50:54 +0800

    Fix: mvn: only collect active modules

--- sbom-tracer/sbom_tracer/local_analyzer/mvn_analyzer.py
+++ sbom-tracer/sbom_tracer/local_analyzer/mvn_analyzer.py
@@ -1,6 +1,10 @@
+import os
+import subprocess
+
 from sbom_tracer.local_analyzer.analyzer_base import AnalyzerBase
-from sbom_tracer.util.common_util import copy_definition_files_recursively
+from sbom_tracer.util.common_util import copy_definition_files
 from sbom_tracer.util.const import MAVEN_DEFINITION_FILE_PATTERNS
+from sbom_tracer.util.shell_util import execute
 
 
 class MvnAnalyzer(AnalyzerBase):
@@ -9,6 +13,11 @@ class MvnAnalyzer(AnalyzerBase):
 
     def _analyze(self, cmd, full_cmd, cwd, fd, task_workspace):
         try:
-            copy_definition_files_recursively(cwd, task_workspace, MAVEN_DEFINITION_FILE_PATTERNS)
+            os.chdir(cwd)
+            paths = [line for line in
+                     execute('mvn -q --also-make exec:exec -Dexec.executable="pwd"', stdout=subprocess.PIPE)[1].strip()
+                     if line.startswith("/") or line.startswith("\\")]
+            for path in paths:
+                copy_definition_files(path, task_workspace, MAVEN_DEFINITION_FILE_PATTERNS)
         except:
             pass
